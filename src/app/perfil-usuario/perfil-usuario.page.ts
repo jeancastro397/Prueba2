be7831a82774de804f182799820769d82877e13f
Jean Castro <je.castrob@duocuc.cl>
2023-11-11 12:06:09 -0300

    Entrega de Proyecto

--- src/app/perfil-usuario/perfil-usuario.page.ts
+++ src/app/perfil-usuario/perfil-usuario.page.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './../modelos/Usuario';
+import { AutorizacionService } from './../servicios/autorizacion.service';
+import { ViewWillEnter, ViewDidLeave } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-perfil-usuario',
   templateUrl: './perfil-usuario.page.html',
   styleUrls: ['./perfil-usuario.page.scss'],
 })
-export class PerfilUsuarioPage implements OnInit {
+export class PerfilUsuarioPage implements ViewWillEnter, ViewDidLeave {
+  public userLogin: Usuario | null = null;
+  private subs: Subscription | null = null;
 
-  constructor() { }
+  constructor(
+    private auth: AutorizacionService
+  ) { }
+
+  ionViewWillEnter(): void {
+    this.subs = this.auth.$usuarioLogeado.subscribe(
+      user => {
+        this.userLogin;
+      }
+    );
+  }
+
+  ionViewDidLeave():void {
+    this.subs?.unsubscribe();
+  }
 
   ngOnInit() {
   }
